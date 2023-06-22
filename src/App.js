a48a0636de9896850cabc9fb2e270c3d5ba94b0a
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-06-23 01:49:28 +0300

    finish trending component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
+import Popular from "./pages/popular";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Sidebar />
+      <Popular />
     </div>
   );
 }
---
+++ src/pages/popular/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TrendingToday from "../../components/trending-today";
+
+function Popular() {
+  return (
+    <section style={{ paddingLeft: "270px" }}>
+      <TrendingToday />
+    </section>
+  );
+}
+
+export default Popular;
